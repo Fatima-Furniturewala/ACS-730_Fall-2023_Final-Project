d30f25bd8cf3bd69736ab6885f01d2e69b785706
Fatima Abubakar Furiturewala <147334922+Fatima-Furniturewala@users.noreply.github.com>
2023-12-10 05:05:47 -0500

    Update output.tf

--- webserver/output.tf
+++ webserver/output.tf
@@ -1,13 +1,13 @@
 output "public_vm1" {
-  value = module.ec2_instances.public_vm1
+  value = module.webserver.public_vm1
 }
 output "bastion_ip" {
-  value = module.ec2_instances.bastion_ip
+  value = module.webserver.bastion_ip
 }
 output "public_vm3_vm4" {
-  value = module.ec2_instances.public_vm3_vm4
+  value = module.webserver.public_vm3_vm4
 }
 output "private_vms_ips" {
-  value = module.ec2_instances.private_vms_ips
+  value = module.webserver.private_vms_ips
 }
 
